ccfe5021502fb3040d06ff4b16999a8e93869972
Dakota Smothers <dsmothers192@gmail.com>
2019-10-16 14:42:14 -0500

    added styling to weather card

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
 (function () {
-    getWeatherInfo();
-
+    //Submit Button Event Handler
+    $("#submit").click(function(){
+        console.log("submit button clicked!");
+    //Get the value the user has entered in the search bar and store it
+    const searchLocation = $("#searchBar").val();
+    //Call the geocode function and pass in the value
+        geocode(searchLocation);
+    //Clear out the search bar
+    $("#searchBar").val("");
+    })
 
 })();
 
 
 //Function to connect to the Dark Sky API and get weather data
-function getWeatherInfo() {
+function getWeatherInfo(latitude, longitude, city, state) {
     //Base-URL/APIKey/Latitude,Longitude
 
-    $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", { dataType: "jsonp"})
+    $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + latitude + "," + longitude, { dataType: "jsonp"})
     .done(function(data) {
         console.log(data);
+
+        //See if you can get the following data from the JSON
+        //1. Get the current temperature
+        console.log(data.currently.temperature);
+
+        //2. Get the probability of precipitation
+        console.log(data.currently.precipProbability);
+        
+        //3. Get the high and low temp for the current day(first element in the data array in the daily object)
+        console.log(daily.data.temperatureHigh);
+        console.log(daily.data.temperatureLow);
     })
     .fail(function(error) {
         console.log(error);
@@ -22,6 +41,33 @@ function getWeatherInfo() {
 }
 
 //Function to connect to the MapQuest Geocoding API and get geocoding data
-function geocode(){
+function geocode(location){
+    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=" + location)
+    .done(function(data){
+        //Get the lat and lng from the response
+        let locations = data.results[0].locations[0];
+
+        let lat = locations.latLng.lat;
+
+        let lng = locations.latLng.lng;
+
+        let city = locations.adminArea5;
+
+        let state = locations.adminArea3;
+
+        //Pass the lat and lng to our getWeatherInfo function
+        getWeatherInfo(lat, lng, city, state);
+
+        console.log(city);
+        console.log(state);
+
+
+    })
+    .fail(function(error){
+        console.log(error);
+    })
+    .always(function(){
+        console.log("Geocoding call finished");
+    })
 
 }
